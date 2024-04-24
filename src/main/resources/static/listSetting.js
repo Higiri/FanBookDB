144b2f41a2f31cbb9587b65201074b5d1a46bce6
Higiri <higiri.abukuma@gmail.com>
2024-04-24 21:20:55 +0900

    [add] list.js暫定対応

---
+++ src/main/resources/static/listSetting.js
@@ -0,0 +1,34 @@
+/**
+ * List.js用のオプション設定
+ */
+var options = {
+    // 並び替え項目
+    valueNames: ["default", "date", "title", "author"],
+
+    page: getDefaultPageSize(),
+    // ページネーション
+    pagination: {
+        innerWindow: 1,
+        outerWindow: 2,
+        paginationClass: "pagination",
+        item: '<li class="page-item"><a class="page page-link" " href="#""></a></li>',
+    },
+};
+
+/**
+ * List.jsのインスタンス生成
+ */
+var fanbooks = new List("fanBookList", options);
+
+// ウィンドウサイズが変更されたときの処理
+window.addEventListener("resize", function () {
+    // ウィンドウサイズに応じてページサイズを変更
+    var newPageSize = getWindowSize() < 768 ? 24 : 96;
+    fanbooks.page = newPageSize;
+    fanbooks.update();
+});
+
+// ウィンドウサイズを取得する関数
+function getWindowSize() {
+    return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+}
